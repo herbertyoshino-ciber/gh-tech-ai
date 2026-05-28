b900ddf9a5a9c477fcbc1a205914f9150eee2353
herbertyoshino-ciber <herbertyoshino@gmail.com>
2026-05-27 23:23:46 -0300

    Update app2.py
    Signed-off-by: herbertyoshino-ciber <herbertyoshino@gmail.com>

--- app2.py
+++ app2.py
@@ -1,4 +1,4 @@
-import os    
+gostaria que aperfeiçoasse esse codigo python para deixar ele muito profissional:       import os    
 import os
 import io
 import re
@@ -441,8 +441,3 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
-       
-
-     
-                
